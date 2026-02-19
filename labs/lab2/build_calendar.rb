99980ef7008dfecd9dc4346795c21377748f81ea
chokopiku61 <tim.23tim23.23@gmail.com>
2026-02-19 21:22:04 +0300

    Доделан скрипт создания расписания

--- labs/lab2/build_calendar.rb
+++ labs/lab2/build_calendar.rb
@@ -3,12 +3,12 @@ require 'date'
 comands = []
 File.open(ARGV[0], 'r') do |file|
   file.each_line do |line|
-    comands << line.gsub(/^\d+\.\s*/, "") 
+    comands << line.gsub(/^\d+\.\s*/, "").strip
   end
 end
 
 d1 = Date.parse(ARGV[1])
-d2 = Date.parse(ARGV[2])
+d2 =  Date.parse(ARGV[2])
 
 all_matchs = []
 comands.combination(2).each do |team1, team2|
@@ -22,7 +22,25 @@ d1.upto(d2) do |date|
   end
 end
 
-puts games_date.size
-days_per_match =  (games_date.size*6) / all_matchs.size.to_f
-puts "Матчей в день (среднее): #{'%.2f' % days_per_match}"
+total_slots = games_date.size * 3 * 2
+if all_matchs.size > total_slots
+  puts "ОШИБКА: Нужно #{all_matchs.size} слотов, доступно #{total_slots}"
+  exit 1
+end
 
+matches_per_day = all_matchs.size.to_f / games_date.size
+puts "Среднне кол-во матчей в день: #{matches_per_day}"
+
+File.open(ARGV[3], 'w') do |f|
+  games_date.each_with_index do |date, i|
+    start_idx = (i * matches_per_day).to_i
+    end_idx = ((i + 1) * matches_per_day).to_i
+    days_matches = all_matchs[start_idx...end_idx]
+    hours =  date.sunday? ? [12, 15, 18] : [18, 15, 12]
+    days_matches.each_with_index do |match, match_idx|
+      f << "#{date} #{hours[match_idx % 3]}:00 |   #{match}\n"
+    end
+  end
+end
+
+puts "Заполение окончено."
